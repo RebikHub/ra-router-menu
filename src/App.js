25abc1ac50b22247875a94cef6b3a9b67b982bdc
Alexandr Rebikov <rebikovlk@mail.ru>
2022-04-01 15:18:47 +0700

    path gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import DriftPage from "./components/DriftPage";
 import ForzaPage from "./components/ForzaPage";
 import HomePage from "./components/HomePage";
@@ -8,18 +8,18 @@ import TimeAttackPage from "./components/TimeAttackPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <div>
           <Menu />
           <div className="page">
             <Routes>
-              <Route path="https://rebikhub.github.io/ra-router-menu/" element={<HomePage/>} />
-              <Route path="https://rebikhub.github.io/ra-router-menu/drift" element={<DriftPage/>} />
-              <Route path="https://rebikhub.github.io/ra-router-menu/timeattack" element={<TimeAttackPage/>} />
-              <Route path="https://rebikhub.github.io/ra-router-menu/forza" element={<ForzaPage/>} />
+              <Route path="/ra-router-menu/" element={<HomePage/>} />
+              <Route path="/ra-router-menu/drift" element={<DriftPage/>} />
+              <Route path="/ra-router-menu/timeattack" element={<TimeAttackPage/>} />
+              <Route path="/ra-router-menu/forza" element={<ForzaPage/>} />
             </Routes>
           </div>
         </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
